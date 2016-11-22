b1079deafe2462541cc67bebeddde79260c327c3
Jacopo Bufalino <jacopobufalino@MacBook-Pro-di-Jacopo.local>
2016-11-22 15:49:35 +0200

    minor fix

--- ketje.c
+++ ketje.c
@@ -53,12 +53,23 @@ void ketje_mj_e(unsigned char *cryptogram,
 			k_len,nonce,n_len,data, d_len,header, h_len);
 
 printf("Calling Wrap again\n");
-MonkeyWrapWrap(D,cryptogram,tag,t_len,data,288,header,h_len);
+MonkeyWrapWrap(D,cryptogram,tag,t_len,data,168,header,h_len);
+printf("Calling third time\n");
+MonkeyWrapWrap(D,cryptogram,tag,t_len,data,0,header,208);
 	//printf("Size of the algorithm is %u\n",D->n_stride);
 	/* Implement this function */
 
 	return;
 }
+/*****************************************************************************
+ * @brief: This set of functions prints the content of a generic state array
+ * @author: Jacopo Bufalino - jacopobufalino@gmail.com
+ * @date: 2016/27/11 
+ * @return: void 
+ * @arg: inpunt state array
+ * @note: Use them just to debug the code
+ *
+ *****************************************************************************/
 
 Duplex*
 DuplexInit(unsigned int f,unsigned int rho,unsigned int n_start,
@@ -75,6 +86,15 @@ DuplexInit(unsigned int f,unsigned int rho,unsigned int n_start,
 }
 
 
+/*****************************************************************************
+ * @brief: This set of functions prints the content of a generic state array
+ * @author: Jacopo Bufalino - jacopobufalino@gmail.com
+ * @date: 2016/27/11 
+ * @return: void 
+ * @arg: inpunt state array
+ * @note: Use them just to debug the code
+ *
+ *****************************************************************************/
 
 void
 MonkeyWrap(Duplex *D, unsigned char *cryptogram,
@@ -97,6 +117,15 @@ MonkeyWrap(Duplex *D, unsigned char *cryptogram,
 	return;
 
 }
+/*****************************************************************************
+ * @brief: This set of functions prints the content of a generic state array
+ * @author: Jacopo Bufalino - jacopobufalino@gmail.com
+ * @date: 2016/27/11 
+ * @return: void 
+ * @arg: inpunt state array
+ * @note: Use them just to debug the code
+ *
+ *****************************************************************************/
 
 void MonkeyWrapInitialize(Duplex *D, unsigned char *key, unsigned int k_len,
 		unsigned char* seq_no,unsigned int seq_len){
@@ -115,10 +144,17 @@ void MonkeyWrapInitialize(Duplex *D, unsigned char *key, unsigned int k_len,
 	//TODO Make the error on the constant suppress.
 
 	if (seq_len != 0){
-		result_len = concatenate(&data_2_feed,result,k_len+16,
-				seq_no,seq_len); 
-		printf("Resulting len before Duplex Start: %lu\n",result_len);
-		
+	unsigned int length;
+		if (seq_len <=  (D->f - k_len - 18)) 
+			length = seq_len;
+		else 
+			length = D->f - (k_len +18);
+		result_len =
+			concatenate(&data_2_feed,result,k_len+16,
+			seq_no,length); 
+		printf("Resulting len of the nonce: %lu == %lu\n",
+				length,D->f - k_len - 18);
+			
 		for (i = 0 ; i < BYTE_LEN(result_len) ; i++)
 			printf("%.2X ",data_2_feed[i]);
 		
@@ -130,6 +166,15 @@ void MonkeyWrapInitialize(Duplex *D, unsigned char *key, unsigned int k_len,
 
 
 }
+/*****************************************************************************
+ * @brief: This set of functions prints the content of a generic state array
+ * @author: Jacopo Bufalino - jacopobufalino@gmail.com
+ * @date: 2016/27/11 
+ * @return: void 
+ * @arg: inpunt state array
+ * @note: Use them just to debug the code
+ *
+ *****************************************************************************/
 
 void MonkeyWrapWrap(Duplex *D,unsigned char *cryptogram, 
 		unsigned char *tag, unsigned int t_len,
@@ -138,28 +183,34 @@ void MonkeyWrapWrap(Duplex *D,unsigned char *cryptogram,
 
 	/* Setup the number of blocks */
 
+	
+
+
+
 	unsigned long plain_blocks = ((d_len/D->rho)+(d_len%D->rho?1:0)),  
 	header_blocks = h_len/D->rho;
 	uint8_t i = 0;
-	unsigned char* data_concatenated;
+	unsigned char* data_concatenated=NULL,*Z=NULL,*crypto = NULL;
 	unsigned long data_size;
-	unsigned long last_block_size = 0,last_plain =  d_len%D->rho ; // famo finta che è zero
+	unsigned long last_block_size = 0,last_plain =  d_len%D->rho;
+	/* TODO*/
+	crypto = calloc(BYTE_LEN(d_len),sizeof(unsigned char));
+
 	printf("****************MonkeyWrapWrap stats***********\n");
 	printf("*\tThere are %lu blocks of text d_len/D->rho \n",plain_blocks);
 	printf("*\tData len d_len is  %lu\n",d_len);
 	printf("*\tD->rho is %u\n",D->rho);
-
+	printf("*\theader length is %u\n",h_len);
 	printf("***********************************************\n");
 
-	if (plain_blocks > 0 )
-		for ( i = 0 ; i < (plain_blocks -2)  ; ++i){
-
-			data_size = concatenate_00(&data_concatenated,
-			&data[i],D->rho+2);
-			DuplexStep(D,data[i],D->r,0);
-			free(data_concatenated);
-
-		}
+	for ( i = 0  ; (i < (plain_blocks -2)) && (plain_blocks > 1) ; ++i)
+	{
+		printf("YOU SHOULD BE NOT HERE\n");
+		data_size = concatenate_00(&data_concatenated,
+		&data[i],D->rho+2);
+		DuplexStep(D,data[i],D->r,0);
+		free(data_concatenated);
+	}
 
 	/*End of first phase */
 
@@ -169,22 +220,31 @@ void MonkeyWrapWrap(Duplex *D,unsigned char *cryptogram,
 	printf("\n");
 
 
-/*
-* TODO I am omitting the cont.d of the algorithm because it is always null
-NB: cryptogram is already allocated
-*/
-
-	// Using some shortcuts to go straight to the point 
-
 	unsigned char * data4second_step = NULL;
 	unsigned int data_len;
 
 	data_len = concatenate_01(&data4second_step,&data[plain_blocks -1],
 			last_block_size);
+	
 	printf("data_len after concatenating_01 is: %u\n",data_len);
 	printf("data is %.2x\n",data4second_step[0]);
-	//header is zero, and the total length is 2! 
-	unsigned char * Z = DuplexStep(D,data4second_step,data_len,0);
+	
+	unsigned int B0 ;
+	if (plain_blocks == 1) B0 = last_plain;
+	else if (plain_blocks > 1) B0 = D->rho;
+	else B0 = 0;
+
+	 Z = DuplexStep(D,data4second_step,data_len,B0);
+	 printf("\n---C0---\n");
+	for ( i = 0 ; i < BYTE_LEN(B0) ; i++)
+	{
+		crypto[i] = data[i] ^ Z[i];
+		//printf("%.2x ",crypto[i]);
+	
+	}
+	free(Z);
+	
+	
 	// at the end it will be equal to Z...
 	printf("\nMONKEYDUPLEX state after stepping the last block of A:\n");
 	for ( i = 0 ; i < (D->f/8) ; ++i)
@@ -193,21 +253,23 @@ NB: cryptogram is already allocated
 
 
 	header_blocks = h_len/D->rho;unsigned char*temp_i = NULL;
-	if (plain_blocks > 0 ){ 
 
-
-	for (i = 0 ; i <= plain_blocks - 2 ; i++)
+	/* computing Ci+i */
+	for (i = 0 ; (i <= plain_blocks - 2 ) && (plain_blocks > 1 ); i++)
 	{
-		concatenate_11(&temp_i,&data[i],D->rho);
+		concatenate_11(&temp_i,&data[i*BYTE_LEN(D->rho)],D->rho);
 
-		DuplexStep(D,temp_i,D->rho+2,D->rho);
+		Z = DuplexStep(D,temp_i,D->rho+2,D->rho);
+		for (unsigned int j = 0 ; j < BYTE_LEN(D->rho) ; j++)
+			crypto[BYTE_LEN(D->rho)*(i+1)+j] = 
+				data[BYTE_LEN(D->rho)*(i+1)+j]^Z[i]; 
 		/*printf("TODO: implement Ci\n");
 		for ( int j = 0 ; j < BYTE_LEN(D->rho) ; ++j)
 		printf("%.2x ",temp_i[j]);
 	printf("\n");*/
 	
 	
-	}}
+	}
 	printf("\nMONKEYDUPLEX statiie after for B......:\n");
 	for ( i = 0 ; i < (D->f/8) ; ++i)
 		printf("%.2x ",D->state[i]);
@@ -244,7 +306,10 @@ printf("\n\n+++++++++TAG+++++++++++\n\n");
 for (int i = 0 ; i < BYTE_LEN(128) ; i++)
 	printf("%.2X ",temp_tag[i]);
 printf("\n");
-
+printf("\n\n+++++++++CRYPTO+++++++++++\n\n");	
+for (int i = 0 ; i < BYTE_LEN(d_len) ; i++)
+	printf("%.2X ",crypto[i]);
+printf("\n");
 
 
 
@@ -298,6 +363,15 @@ printf("\n");
 
 
 
+/*****************************************************************************
+ * @brief: This set of functions prints the content of a generic state array
+ * @author: Jacopo Bufalino - jacopobufalino@gmail.com
+ * @date: 2016/27/11 
+ * @return: void 
+ * @arg: inpunt state array
+ * @note: Use them just to debug the code
+ *
+ *****************************************************************************/
 
 void
 DuplexStart(Duplex *D,unsigned char *I,unsigned long i_len){
@@ -336,37 +410,38 @@ DuplexStart(Duplex *D,unsigned char *I,unsigned long i_len){
 	   printf("\n\n");
 	  
 }
+/*****************************************************************************
+ * @brief: This set of functions prints the content of a generic state array
+ * @author: Jacopo Bufalino - jacopobufalino@gmail.com
+ * @date: 2016/27/11 
+ * @return: void 
+ * @arg: inpunt state array
+ * @note: Use them just to debug the code
+ *
+ *****************************************************************************/
 
 unsigned char*
 DuplexStep(Duplex *D, unsigned char *sigma,unsigned long s_len,
 		unsigned long l){
 
-	/* rho must be less than l and sigma len */
-	//if (l > D->rho || s_len > D->rho) exit(EXIT_FAILURE);
-	unsigned char *pad,*P,*Pc;
+	unsigned char *pad=NULL,*P=NULL;
 	unsigned long pad_len,P_len,Pc_len;
-
+	/* concatenate padding to the input string sigma*/
 	pad_len = pad10x1(&pad, D->r,s_len);
 	P_len = concatenate(&P,sigma,s_len,pad,pad_len);
 
-	for (unsigned int i = 0 ; i < P_len/8 +1 ; i++)
+	/*for (unsigned int i = 0 ; i < P_len/8 +1 ; i++)
 		printf("%.2x ",P[i]);
-	printf("\n");
+	printf("\n");*/
 
-
-	/* Now I have to concatenate b-r zeros */
-	//Pc = calloc((P_len+(D->f - D->r))/8,sizeof(unsigned char));
-	//memcpy(Pc,&P,P_len/8);
-	/* It could also be unuseful ... TODO check*/
-	for (uint8_t i = 0 ; i < BYTE_LEN(P_len)  ; ++i ) {
+	/* xor the current state with the new string  */
+	for (uint8_t i = 0 ; i < BYTE_LEN(P_len)  ; ++i ) 
 		D->state[i] = D->state[i] ^ P[i];
-
-
-	}
+/*	
 printf("After the xor\n");
 	for (unsigned int i = 0 ; i < 200 ; i++)
 		printf("%.2x ",D->state[i]);
-	printf("\n");
+	printf("\n");*/
 
 
 
@@ -376,52 +451,60 @@ printf("After the xor\n");
 	printf("*\tPadding len is %lu\n",pad_len);
 	printf("*\tP string len is %lu\n",P_len);
 	printf("*\tb-r is %lu\n",D->f- D->r);
-
 	printf("****************************************\n");
 
-	unsigned char * state = keccak_p_star(D->state,D->rho,D->n_step,D->f);
-	free(D->state);
-	free(pad);
-	free(P);
-	free(Pc);
+	/* apply keccak_p_star function */
+	unsigned char * state = keccak_p_star(D->state,D->rho,
+			D->n_step,D->f);
 
+	/* copy the output of keccak to the current state */
+	free(D->state);
 	D->state = state;
+	/* duplicate the state array and truncate it up to l bits */
 	char * ret_val = calloc(BYTE_LEN(l),sizeof(char));
 	memcpy(ret_val,state,BYTE_LEN(l)*sizeof(char));
+	/* free the memory */
+	free(pad);
+	free(P);
+
+       return ret_val;
 
 
 
 }
+/*****************************************************************************
+ * @brief: This set of functions prints the content of a generic state array
+ * @author: Jacopo Bufalino - jacopobufalino@gmail.com
+ * @date: 2016/27/11 
+ * @return: void 
+ * @arg: inpunt state array
+ * @note: Use them just to debug the code
+ *
+ *****************************************************************************/
 
 unsigned char*
 DuplexStride(Duplex *D, unsigned char *sigma,unsigned long s_len,
 		unsigned long l){
-
-	/* rho must be less than l and sigma len */
-	if (l > D->rho || s_len > D->rho) exit(EXIT_FAILURE);
-	unsigned char *pad,*P,*Pc;
+	
+	
+	unsigned char *pad=NULL,*P=NULL;
 	unsigned long pad_len,P_len,Pc_len;
-
+	/* concatenate padding to the input string sigma*/
 	pad_len = pad10x1(&pad, D->r,s_len);
 	P_len = concatenate(&P,sigma,s_len,pad,pad_len);
 
-	for (unsigned int i = 0 ; i < P_len/8 +1 ; i++)
+	/*for (unsigned int i = 0 ; i < P_len/8 +1 ; i++)
 		printf("%.2x ",P[i]);
-	printf("\n");
+	printf("\n");*/
 
-
-	/* Now I have to concatenate b-r zeros */
-	//Pc = calloc((P_len+(D->f - D->r))/8,sizeof(unsigned char));
-	//memcpy(Pc,&P,P_len/8);
-	/* It could also be unuseful ... TODO check*/
-	for (uint8_t i = 0 ; i < BYTE_LEN(P_len)  ; ++i ) {
-	D->state[i] = D->state[i] ^ P[i];
-//	printf("%.2x ^  %.2x = %.2x\n", D->state[i], P[i],D->state[i] ^ P[i]);
-	}
+	/* xor the current state with the new string  */
+	for (uint8_t i = 0 ; i < BYTE_LEN(P_len)  ; ++i ) 
+		D->state[i] = D->state[i] ^ P[i];
+/*	
 printf("After the xor\n");
 	for (unsigned int i = 0 ; i < 200 ; i++)
 		printf("%.2x ",D->state[i]);
-	printf("\n");
+	printf("\n");*/
 
 
 
@@ -431,65 +514,67 @@ printf("After the xor\n");
 	printf("*\tPadding len is %lu\n",pad_len);
 	printf("*\tP string len is %lu\n",P_len);
 	printf("*\tb-r is %lu\n",D->f- D->r);
-
 	printf("****************************************\n");
 
+	/* apply keccak_p_star function */
 	unsigned char * state = keccak_p_star(D->state,D->rho,
 			D->n_stride,D->f);
 
-	printf("After keccak\n");
-	
+	/* copy the output of keccak to the current state */
 	free(D->state);
-	
-	
-	
-	free(pad);
-	free(P);
-	free(Pc);
-
 	D->state = state;
-
-	//memcpy(S_cut,&D->state,l/8);
+	/* duplicate the state array and truncate it up to l bits */
 	char * ret_val = calloc(BYTE_LEN(l),sizeof(char));
 	memcpy(ret_val,state,BYTE_LEN(l)*sizeof(char));
+	/* free the memory */
+	free(pad);
+	free(P);
+
        return ret_val;
 }
 
 
 
 
-
-
+/*****************************************************************************
+ * @brief: This function implements the Keypack algorithm (see sec. 1.4)
+ * @author: Jacopo Bufalino - jacopobufalino@gmail.com
+ * @date: 2016/27/11 
+ * @return: void 
+ * @arg: Key - keysize - alignment value
+ *
+ *****************************************************************************/
 void
 keypack(unsigned char** result,const unsigned char *key,unsigned long n_bits,
 		unsigned long l){
 	if ( n_bits > (255*8) || n_bits%8 ) exit(EXIT_FAILURE);
 	
-	int i;
-	unsigned char*res = NULL;
-	unsigned char *pad_key=NULL;
-	unsigned long result_size;
+
+	unsigned char*res = NULL,*simple_pad = NULL,*pad_key=NULL;
+	unsigned long result_size,l_conc;
 	uint8_t B_val = l/8, padding=0x01;
+	/* concatenate enc(l/8) with the key */
+	result_size = concatenate(&pad_key,&B_val, 8 ,key, n_bits);
+	if (result_size <= 0 ) exit(EXIT_FAILURE);
+	/* concatenate 0x01 to the previous string */
+	result_size = concatenate(&res,pad_key,n_bits +8, &padding,8);
+	/* create an empty array of the desired size l-result_size 
+	 * and concatenate it to the previous one to get the final
+	 * result */
+	simple_pad =
+		calloc(BYTE_LEN(l-result_size),sizeof(unsigned char));
+	
+	l_conc = 
+		concatenate(result,res,result_size,simple_pad,l-result_size);
+	printf("Len of the array after keypack is: %lu\n",l_conc);
 	printf("****************Keypack stats***********\n");
 	printf("*\tKeylen is %lu bits\n",n_bits);
 	printf("*\tKeylen is %u bytes\n",BYTE_LEN(n_bits));
 	printf("*\tThe first byte of the new array should be %.2X\n",B_val);
 	printf("****************************************\n");
-	// you cannot have  %8 != 0
-	//printf("Concatenating key with its value in bytes\n");
-	result_size = concatenate(&pad_key,&B_val, 8 ,key, n_bits);
-	if (result_size <= 0 ) exit(EXIT_FAILURE);
-	result_size = concatenate(&res,pad_key,n_bits +8, &padding,8);
-	
-	unsigned char* simple_pad =
-		calloc(BYTE_LEN(l-result_size),sizeof(unsigned char));
-	simple_pad[0] = 0x01;
-	unsigned long l_conc = concatenate(result,res,result_size,simple_pad,l-result_size);
-	printf("Len of the array after keypack is: %lu\n",l_conc);
-
-	
 	free(res);
 	free(pad_key);
+	free(simple_pad);
 	return;
 }
 
